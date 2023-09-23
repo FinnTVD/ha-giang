059ac4afd9a83ec23d75092662252b0e5b86b190
FinnTVD <finnit.th@gmail.com>
2023-09-23 18:45:10 +0700

    update

---
+++ components/homepage/Family.js
@@ -0,0 +1,25 @@
+import SubTitle from '../global/SubTitle'
+
+export default function Family() {
+    return (
+        <section className='px-[6.25vw] flex'>
+            <div className=''>
+                <SubTitle
+                    title={'CHEERS'}
+                    subTitle={'FAMILY'}
+                />
+                <h2 className='mt-[1.88vw] mb-[0.5vw] text-[1.625vw] font-semibold leading-[1.23] text-gray-scale-50 uppercase'>
+                    H’MONG TEAM
+                </h2>
+                <p>
+                    Blown away by our Tanzania safari, I never expected it to be that good — it totally exceeded my
+                    expectations and was all hassle free. Our guide was absolutely amazing too. Have re-booked and
+                    cannot wait for next year! Blown away by our Tanzania safari, I never expected it to be that good —
+                    it totally exceeded my expectations and was all hassle free. Our guide was absolutely amazing too.
+                    Have re-booked and cannot wait for .Our guide was absolutely amazing too.{' '}
+                </p>
+            </div>
+            <div className=''></div>
+        </section>
+    )
+}
