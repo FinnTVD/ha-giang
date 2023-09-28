5571891d8112fd022bf18c67260aadda2291eadc
SyxNeet <110085910+SyxNeet@users.noreply.github.com>
2023-09-28 07:35:20 -0700

    responsive the trip

--- components/ui/AccordionDemo.jsx
+++ components/ui/AccordionDemo.jsx
@@ -1,11 +1,11 @@
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from './accordion'
 
-export function AccordionDemo() {
+export function AccordionDemo({className}) {
     return (
         <Accordion
             type='single'
             collapsible
-            className='w-full'
+            className={`w-full ${className}`}
         >
             <AccordionItem value='item-1'>
                 <AccordionTrigger>Is it accessible?</AccordionTrigger>
