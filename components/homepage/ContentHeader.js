5483424860bad43d5bfee6302a71506f9af469f1
FinnTVD <finnit.th@gmail.com>
2023-10-08 14:22:50 +0700

    animation text

--- components/homepage/ContentHeader.js
+++ components/homepage/ContentHeader.js
@@ -1,15 +1,24 @@
+'use client'
+
+import IconTextHeader from '../icons/IconTextHeader'
+
 export default function ContentHeader({ header }) {
     return (
-        <div className='absolute top-1/3 left-1/4 max-md:left-[5.38vw]'>
+        <div
+            id='slogan-header'
+            className='absolute top-1/3 left-1/4 max-md:left-[5.38vw]'
+        >
             <div
                 id='box-slogan'
-                className='relative flex flex-col text-white'
+                className='relative flex flex-col text-white xl:hidden'
                 dangerouslySetInnerHTML={{ __html: header?.slogan }}
             />
-            {/* <h1 className='text-[4vw] font-heavitas font-normal leading-normal max-md:text-[6.4vw]'>BACKPACK UP</h1>
-                <h2 className='font-tomatoes text-[8vw] max-md:text-[16vw] font-normal leading-normal absolute left-[60%] top-[30%] whitespace-nowrap'>
-                    and go
-                </h2> */}
+            <h2 className='text-[4vw] font-heavitas text-white font-normal leading-normal max-md:text-[6.4vw] max-xl:hidden'>
+                BACKPACK UP
+            </h2>
+            <div className='font-tomatoes text-[8vw] max-md:text-[16vw] font-normal leading-normal absolute left-[60%] top-[30%] whitespace-nowrap max-xl:hidden'>
+                <IconTextHeader />
+            </div>
         </div>
     )
 }
