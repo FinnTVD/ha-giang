a55acaabcfa59da2ce73acdd926ac63fc86d782d
FinnTVD <finnit.th@gmail.com>
2023-10-13 20:50:58 +0700

    fix slide

---
+++ components/homepage/ItemVideo.js
@@ -0,0 +1,42 @@
+'use client'
+import { useEffect, useState } from 'react'
+import IconMuted from '../icons/IconMuted'
+import IconAudio from '../icons/IconAudio'
+
+export default function ItemVideo({ e, index, indexSlider }) {
+  const [isMute, setIsMute] = useState(true)
+
+  useEffect(() => {
+    setIsMute(true)
+  }, [indexSlider])
+
+  return (
+    <>
+      <video
+        autoPlay
+        poster='/images/bannervn.jpg'
+        loop
+        className='w-full h-full lg:rounded-[1rem] object-cover min-w-full min-h-full'
+        id={'videoBanner' + index}
+        playsInline
+        muted={isMute}
+      >
+        <source
+          type='video/mp4'
+          src={e?.linkVideo?.url || '/images/video1.mp4'}
+        ></source>
+        Your browser does not support HTML5 video.
+      </video>
+      <div
+        onClick={() => setIsMute(!isMute)}
+        className='absolute bottom-[3.75rem] right-[6.25rem] z-[99] cursor-pointer max-md:right-[4.27rem] max-md:bottom-[5.23rem] max-lg:right-[6.25rem] max-lg:bottom-[6.75rem]'
+      >
+        {isMute ? (
+          <IconMuted className='lg:w-[2.5rem] lg:h-[2.5rem] max-md:w-[10.67rem] max-md:h-[10.67rem] max-lg:w-[8rem] max-lg:h-[8rem]' />
+        ) : (
+          <IconAudio className='lg:w-[2.5rem] lg:h-[2.5rem] max-md:w-[10.67rem] max-md:h-[10.67rem] max-lg:w-[8rem] max-lg:h-[8rem]' />
+        )}
+      </div>
+    </>
+  )
+}
--- components/homepage/SlideBanner.js
+++ components/homepage/SlideBanner.js
@@ -5,13 +5,11 @@ import Image from 'next/image'
 import { useEffect, useRef, useState } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { EffectFade } from 'swiper/modules'
-import IconMuted from '../icons/IconMuted'
-import IconAudio from '../icons/IconAudio'
+import ItemVideo from './ItemVideo'
 
 let arrSlide = []
 export default function SlideBanner({ section1, isMobile }) {
   const swiperRef = useRef(null)
-  const [isMute, setIsMute] = useState(true)
   const [indexSlider, setIndexSlider] = useState(0)
   const handleSlideChange = (swiper) => {
     setIndexSlider(swiper.realIndex)
@@ -47,22 +45,15 @@ export default function SlideBanner({ section1, isMobile }) {
       >
         {arrSlide?.length &&
           arrSlide?.map((e, index) => (
-            <SwiperSlide key={index}>
-              <video
-                autoPlay
-                poster='/images/bannervn.jpg'
-                loop
-                className='w-full h-full lg:rounded-[1rem] object-cover min-w-full min-h-full'
-                id={'videoBanner' + index}
-                playsInline
-                muted={isMute}
-              >
-                <source
-                  type='video/mp4'
-                  src={e?.linkVideo?.url || '/images/video1.mp4'}
-                ></source>
-                Your browser does not support HTML5 video.
-              </video>
+            <SwiperSlide
+              key={index}
+              className='relative'
+            >
+              <ItemVideo
+                e={e}
+                index={index}
+                indexSlider={indexSlider}
+              />
             </SwiperSlide>
           ))}
       </Swiper>
@@ -151,16 +142,6 @@ export default function SlideBanner({ section1, isMobile }) {
             </span>
           </div>
         </div>
-      </div>
-      <div
-        onClick={() => setIsMute(!isMute)}
-        className='absolute bottom-[3.75rem] right-[6.25rem] z-20 cursor-pointer max-md:right-[4.27rem] max-md:bottom-[5.23rem] max-lg:right-[6.25rem] max-lg:bottom-[6.75rem]'
-      >
-        {isMute ? (
-          <IconMuted className='lg:w-[2.5rem] lg:h-[2.5rem] max-md:w-[10.67rem] max-md:h-[10.67rem] max-lg:w-[8rem] max-lg:h-[8rem]' />
-        ) : (
-          <IconAudio className='lg:w-[2.5rem] lg:h-[2.5rem] max-md:w-[10.67rem] max-md:h-[10.67rem] max-lg:w-[8rem] max-lg:h-[8rem]' />
-        )}
       </div>
     </div>
   )
