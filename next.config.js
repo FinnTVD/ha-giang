9290ce0af507e8e0d86d183b8a29c3858633ea77
FinnTVD <finnit.th@gmail.com>
2023-10-17 00:28:41 +0700

    fix line clamp in slide people

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        formats: ['image/avif', 'image/webp'],
-        minimumCacheTTL: 60,
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'cheers-cms.okhub.tech',
-            },
-        ],
-    }
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    minimumCacheTTL: 60,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cheers.okhub.tech',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
