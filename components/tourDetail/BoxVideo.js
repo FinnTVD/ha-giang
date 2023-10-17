d3278e82a4f58d01e47b24ca104ddd037b388e34
FinnTVD <finnit.th@gmail.com>
2023-10-17 17:54:09 +0700

    update

--- components/tourDetail/BoxVideo.js
+++ components/tourDetail/BoxVideo.js
@@ -1,16 +1,18 @@
 export default function BoxVideo({ data }) {
   return (
     <section className='mx-auto lg:w-[66rem] lg:h-[37rem] md:w-[87.5rem] h-[47rem] mt-[6.25rem] max-md:px-0 max-md:mt-[5.3rem] max-md:h-[56rem] w-full'>
-      <iframe
-        width='560'
-        height='315'
-        src={data?.videoTour?.url || 'https://www.youtube.com/embed/Nj6IqVLoaUg?si=RMqLq4uUSDQRlV-Y'}
-        title='YouTube video player'
-        frameborder='0'
-        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-        allowfullscreen
-        className='object-cover w-full h-full rounded-[1.25rem] max-md:rounded-none'
-      ></iframe>
+      <div className='w-full h-full'>
+        <iframe
+          width='560'
+          height='315'
+          src={data?.videoTour?.url || 'https://www.youtube.com/embed/Nj6IqVLoaUg?si=RMqLq4uUSDQRlV-Y'}
+          title='YouTube video player'
+          frameborder='0'
+          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+          allowfullscreen
+          className='object-cover w-full h-full rounded-[1.25rem] max-md:rounded-none'
+        ></iframe>
+      </div>
     </section>
   )
 }
