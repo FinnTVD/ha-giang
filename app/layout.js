f434dccf687fc8ce00ae3eadd97f9371e50db5a0
FinnTVD <finnit.th@gmail.com>
2023-10-17 16:49:49 +0700

    fix

--- app/layout.js
+++ app/layout.js
@@ -60,6 +60,12 @@ export default function RootLayout({ children }) {
           suppressHydrationWarning={true}
           className={`bg-white text-black ${poppins.className} ${heavitas.className} ${tomatoes.className} ${roboto.className} w-full`}
         >
+          <head>
+            <meta
+              name='viewport'
+              content='width=device-width, initial-scale=1, maximum-scale=1'
+            ></meta>
+          </head>
           <AosInit />
           <Theme>
             {children}
