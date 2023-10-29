7ef53b35fb14ff1559fc8de7044e3fdca33de9d2
sonnt <ngotheson1@gmail.com>
2023-10-29 21:27:40 +0700

    fix: performance

--- app/(home)/page.js
+++ app/(home)/page.js
@@ -1,6 +1,19 @@
-import IndexHomePage from '@/components/homepage'
 import { GET_DATA_HOME, GET_META_HOME } from '@/graphql/home/queries'
 import getData from '@/utils/getData'
+import Image from 'next/image'
+
+import dynamic from 'next/dynamic'
+
+const CheersTour = dynamic(() => import('../../components/homepage/CheersTour'));
+const TheGallery = dynamic(() => import('../../components/homepage/TheGallery'));
+const Travelers = dynamic(() => import('../../components/homepage/Travelers'));
+const Weather = dynamic(() => import('../../components/homepage/Weather'));
+const TheTrip = dynamic(() => import('../../components/homepage/TheTrip'));
+const GreatTrips = dynamic(() => import('../../components/homepage/GreatTrips'));
+const Family = dynamic(() => import('../../components/homepage/Family'));
+const Header = dynamic(() => import('../../components/global/Header'));
+const Banner = dynamic(() => import('../../components/homepage/Banner'));
+
 export async function generateMetadata() {
   const data = await getData(GET_META_HOME)
   if (!data) return
@@ -55,11 +68,54 @@ export async function generateMetadata() {
   }
 }
 export default async function page() {
-  const data = await getData(GET_DATA_HOME)
+  const res = await getData(GET_DATA_HOME)
+  const allTourHG = res?.data?.allTourHG
+  const data = res?.data?.page?.homeHG
+  const header = data?.header
+  const section1 = data?.section1
+  const section2 = data?.section2
+  const section3 = data?.section3
+  const section4 = data?.section4
+  const section5 = data?.section5
+  const section6 = data?.section6
+  const section7 = data?.section7
+  const section8 = data?.section8
+
   return (
-    <IndexHomePage
-      data={data?.data?.page?.homeHG}
-      allTourHG={data?.data?.allTourHG}
-    />
+    <>
+      <Header
+        header={header}
+        allTourHG={allTourHG}
+        isHome={true}
+      />
+      <main className='relative w-full overflow-x-hidden'>
+        <Banner section1={section1} />
+        <Image
+          className='!h-[220vh] object-cover w-full top-0 left-0 z-0 max-lg:hidden'
+          src={'/images/mask.png'}
+          fill
+          sizes='100vw'
+        />
+        <CheersTour
+          section2={section2}
+          allTourHG={allTourHG}
+        />
+        <GreatTrips
+          section3={section3}
+          allTourHG={allTourHG}
+        />
+        <TheGallery
+          section4={section4}
+          allTourHG={allTourHG}
+        />
+        <Travelers section5={section5} />
+        <Family section6={section6} />
+        <Weather section7={section7} />
+        <TheTrip
+          section8={section8}
+          allTourHG={allTourHG}
+        />
+      </main>
+    </>
   )
 }
