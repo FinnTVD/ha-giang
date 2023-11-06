9c0dd64783120854c1e0eb26e96792410acbf5b3
sonnt <sonnt@toprate.io>
2023-11-06 15:09:28 +0700

    feat: property for web

--- app/about-us/page.jsx
+++ app/about-us/page.jsx
@@ -1,6 +1,9 @@
+import { GET_DETAIL_TOUR } from '@/graphql/tourDetail/queries'
+import getData from '@/utils/getData'
+import Link from 'next/link'
 import React from 'react'
 
-const AboutUs = async () => {
+const AboutUs = async ({params}) => {
   const data = await getData(GET_DETAIL_TOUR, { slug: params?.slug })
   // if (!data?.data?.tourHG) return <IndexNotFound />
   return (
