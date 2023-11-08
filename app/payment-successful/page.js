71f0a4b192eed5cf2658b1d70365b7e2e47f7b03
sonnt <sonnt@toprate.io>
2023-11-08 17:46:54 +0700

    fix build

--- app/payment-successful/page.js
+++ app/payment-successful/page.js
@@ -1,3 +1,5 @@
+import PaymentSuccessFulPage from "@/components/payment-successful";
+
 export default function PaymentSuccessFul({ searchParams }) {
   return <PaymentSuccessFulPage searchParams={searchParams} />
 }
