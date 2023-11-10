b46fe38ea9091bf61e391cec328e46cd9a1753d4
sonnt <sonnt@toprate.io>
2023-11-10 09:23:13 +0700

    fix: build

--- utils/getData.js
+++ utils/getData.js
@@ -1,26 +1,60 @@
-import 'server-only'
+// import 'server-only'
 
-export default async function getData(query, variables = {}) {
-  const res = await fetch(process.env.API, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      query,
-      variables,
-    }),
-    next: { revalidate: 60 },
-  })
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
+// export default async function getData(query, variables = {}) {
+//   const res = await fetch(process.env.API, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify({
+//       query,
+//       variables,
+//     }),
+//     next: { revalidate: 60 },
+//   })
+//   // The return value is *not* serialized
+//   // You can return Date, Map, Set, etc.
 
-  // Recommendation: handle errors
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data')
-    // return null
+//   // Recommendation: handle errors
+//   if (!res.ok) {
+//     // This will activate the closest `error.js` Error Boundary
+//     throw new Error('Failed to fetch data')
+//     // return null
+//   }
+
+//   return res.json()
+// }
+
+import fetchPonyfill from 'fetch-ponyfill'
+
+export default async function getData(
+  query, variables = {},
+  usePonyfill = true
+) {
+  try {
+    const res = await (usePonyfill ? fetchPonyfill() : { fetch }).fetch(process.env.NEXT_PUBLIC_API, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query,
+        variables,
+      }),
+      next: { revalidate: 60 },
+    })
+    // The return value is *not* serialized
+    // You can return Date, Map, Set, etc.
+
+    // Recommendation: handle errors
+    if (!res.ok) {
+      // This will activate the closest `error.js` Error Boundary
+      throw new Error('Failed to fetch data')
+      // return null
+    }
+
+    return res.json()
+  } catch (error) {
+    throw new Error('Failed to fetch data', error)
   }
-
-  return res.json()
 }
