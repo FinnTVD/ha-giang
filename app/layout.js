d6ddad60f626c0ed877fec889fe29809f922724b
sonnt <sonnt@toprate.io>
2023-11-13 09:41:53 +0700

    fix: errror page

--- app/layout.js
+++ app/layout.js
@@ -95,7 +95,7 @@ export default function RootLayout({ children }) {
           <AosInit />
           <Theme>
             {children}
-            <Footer />
+            {/* <Footer /> */}
           </Theme>
         </ApolloWrapper>
         <Script
