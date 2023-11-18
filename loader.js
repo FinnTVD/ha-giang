ced242bd07b4e48136cb54f86442711711be6626
FinnTVD <finnit.th@gmail.com>
2023-11-18 16:56:44 +0700

    fix img

--- loader.js
+++ loader.js
@@ -1,25 +1,27 @@
 const isProd = process.env.NODE_ENV === 'production'
 export default function myImageLoader({ src, width, quality }) {
+  return `${src}?w=${width}&q=${quality || 75}`
   // Run development
-  if (!isProd) return `${src}?w=${width}&q=${quality || 75}`
+  // if (!isProd) return `${src}?w=${width}&q=${quality || 75}`
 
-  // Run Production
-  // Public file
-  if (!src.includes(process.env.NEXT_PUBLIC_ORIGIN_URL)) {
-    let url = src
-    if (!!src.includes('/_next/static/media/')) {
-      let urls = src.split('.')
-      urls.splice(-2, 1)
-      url = urls.join('.')
-      url = url.replace('/_next/static/media/', '')
-    } else {
-      url = url.replace('/images/', '')
-    }
+  // // Run Production
+  // // Public file
+  // if (src.includes("wp-content/uploads")) return `${src}?w=${width}&q=${quality || 75}`
+  // if (!src.includes(process.env.NEXT_PUBLIC_ORIGIN_URL)) {
+  //   let url = src
+  //   if (!!src.includes('/_next/static/media/')) {
+  //     let urls = src.split('.')
+  //     urls.splice(-2, 1)
+  //     url = urls.join('.')
+  //     url = url.replace('/_next/static/media/', '')
+  //   } else {
+  //     url = url.replace('/images/', '')
+  //   }
 
-    return `${process.env.NEXT_PUBLIC_CDN_URL}/public/${url}`
-  }
+  //   return `${process.env.NEXT_PUBLIC_CDN_URL}/public/${url}`
+  // }
 
-  // Other file
-  const url = src.replace(process.env.NEXT_PUBLIC_ORIGIN_URL, process.env.NEXT_PUBLIC_CDN_URL)
-  return url
+  // // Other file
+  // const url = src.replace(process.env.NEXT_PUBLIC_ORIGIN_URL, process.env.NEXT_PUBLIC_CDN_URL)
+  // return url
 }
