458d56c11d3ee2cec6ff089bc151f412bdc595ec
FinnTVD <finnit.th@gmail.com>
2023-12-27 22:29:11 +0700

    add

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    deviceSizes: [360, 375, 390, 430, 768, 992, 1080, 1200, 1480, 1600, 1920, 2048, 2600],
+    deviceSizes: [360, 375, 390, 412, 430, 768, 992, 1080, 1200, 1480, 1600, 1920, 2048, 2600],
     formats: ['image/avif', 'image/webp'],
     minimumCacheTTL: 3600,
     dangerouslyAllowSVG: true,
