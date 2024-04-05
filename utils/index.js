5ddb445a1bc15cd8b5b7267f15bf1c9a8a023ca7
FinnTVD <finnit.th@gmail.com>
2024-04-05 10:02:35 +0700

    update rate dollar

--- utils/index.js
+++ utils/index.js
@@ -24,7 +24,7 @@ const notifyError = () =>
     theme: 'colored',
   })
 
-const exchangeRate = 24800
+const exchangeRate = 25000
 const sizeBlog = 12
 
 export { exchangeRate, notify, notifyError, sizeBlog }
