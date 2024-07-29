27ad28f5ed6b9e2ffa8499318b42a909f9cd747f
FinnTVD <finnit.th@gmail.com>
2024-07-29 16:45:01 +0700

    update

--- next.config.js
+++ next.config.js
@@ -3,10 +3,10 @@ const nextConfig = {
   images: {
     // deviceSizes: [360, 375, 390, 412, 430, 640, 750, 992, 1080, 1200, 1480, 1600, 1920, 2048, 2600],
     formats: ['image/webp'],
-    minimumCacheTTL: 3600,
-    dangerouslyAllowSVG: true,
-    contentDispositionType: 'attachment',
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    minimumCacheTTL: 60,
+    // dangerouslyAllowSVG: true,
+    // contentDispositionType: 'attachment',
+    // contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     remotePatterns: [{ protocol: 'https', hostname: '**' }],
   },
   compiler: {
